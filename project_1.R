e16d678476851af2047e37a950c387b52f1e2b75
ekusnadi <ekusnadi@berkeley.edu>
2025-09-24 18:56:23 -0700

    removed unnecessary lines

--- project_1.R
+++ project_1.R
@@ -9,31 +9,43 @@ sjsu_blue <- "#0055A2"
 sjsu_gold <- "#E5A823"
 sjsu_gray <- "#939597"
 sjsu_darkgray <- "#53565A"
-# Scatter plot
-par(mfrow = c(1, 1))
-plot(education, tox_release,
-     col = sjsu_blue,
-     pch = 19,
-     xlab = "Percent of Population (25+) with Less Than High School Education",
-     ylab = "Toxicity-weighted Concentrations of Chemical Releases",
-     main = "Toxic Releases vs. Educational Attainment",
-     cex.main = 1.2, cex.lab = 1.1)
-clean_data_no_outlier <- clean_data[clean_data$`Tox. Release` <= 40000, ]
-education_no_outlier <- clean_data_no_outlier$Education
-tox_release_no_outlier <- clean_data_no_outlier$`Tox. Release`
+
 # Histograms side by side
 par(mfrow = c(1, 2))
-hist(education_no_outlier,
+hist(education,
      main = "Distribution of Educational Attainment",
      xlab = "Percent of Population (25+) with Less Than HS Education",
      col = sjsu_blue,
      border = "white")
-hist(tox_release_no_outlier,
+hist(tox_release,
      main = "Distribution of Toxic Releases",
      xlab = "Toxicity-weighted Concentrations of Chemical Releases",
      col = sjsu_gold,
      border = "white")
-# Reset plotting area
+
 par(mfrow = c(1, 1))
+
 # linear regression
-model <- lm(tox_release_no_outlier ~ education_no_outlier)
+model <- lm(tox_release ~ education)
+
+# scatter plot
+plot(education, tox_release,
+     col = sjsu_blue,
+     pch = 16,
+     xlab = "Percent of Population (25+) with Less Than High School Education",
+     ylab = "Toxicity-weighted Concentrations of Chemical Releases",
+     main = "Toxic Releases vs. Educational Attainment",
+     cex.main = 1.2, cex.lab = 1.1)
+abline(model, col = "red", lwd = 2)
+
+# residuals vs fitted
+fitted_values <- fitted(model)
+residuals_values <- residuals(model)
+
+plot(fitted_values, residuals_values,
+     col = sjsu_blue,
+     main = "Residuals vs Fitted Values",
+     xlab = "Fitted Values",
+     ylab = "Residuals",
+     pch = 16, )
+abline(h = 0)
